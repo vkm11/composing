155edc309402924a8365e97feb2336bdf3d48fde
vijaykumar <vijay1111mane@gmail.com>
2022-05-24 14:51:51 +0530

    commite;

---
+++ src/components/Home.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h1>Home Component</h1>
+    <Users/>
+    <Products />
+  </div>
+</template>
+<script>
+import Users from './Users'
+import Products from './Products'
+
+export default {
+  name:"vue-home",
+  components:{
+    Users,
+    Products
+}
+}
+</script>
+<style>
+div{ 
+    background-color: #dde5ec;
+    height: 500px
+}
+</style>
---
+++ src/components/Users.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="users">
+    <h1>Users Component</h1>
+    
+  </div>
+</template>
+<script>
+
+export default {
+  name:"vue-Users",
+ 
+  
+}
+</script>
+<style>
+.users{ 
+    background-color: antiquewhite;
+    height: 100px;
+    width: 300px;
+    padding: 42px;
+    margin: 20px;
+}
+</style>
